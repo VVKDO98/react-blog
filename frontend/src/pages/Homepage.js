11d39a07668ee97f54ad9356d68894a87dc1a6b3
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-28 10:48:17 +0200

    Fetch api

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
+import {Link} from 'react-router-dom';
 
 const Homepage = () => {
+    const {loading, error, data} = useFetch('http://localhost:1337/api/articles')
+
+    if(loading) return <p>Loading ...</p>
+    if(error) return <p>Error ...</p>
+    console.log(data.data)
+
     return (
         <div>
-            <h1>Homepage</h1>
+            {data.map((article) => (
+                <div key={article.id}>
+                    <h2>{article.attributes.title}</h2>
+                    <small>console list</small>
+                    <p>{article.attributes.content.substring(0, 200)}...</p>
+                    <Link to={`/details/${article.id}`}>Read more</Link>
+                </div>
+            ))}
         </div>
     );
 };
