162313dcaf9b29b697f0657f8f95f21930425ac2
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-10-28 10:18:19 +0200

    Add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import SiteHeader from "./components/SiteHeader";
+import {Routes, Route} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import ArticleDetails from "./pages/ArticleDetails";
+import Category from "./pages/Category";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+      <div className="App">
+        <SiteHeader/>
+        <Routes>
+          <Route exact path="/" element={<Homepage/>}/>
+          <Route path="/details/:id" element={<ArticleDetails/>}/>
+          <Route path="/category/:id" element={<Category/>}/>
+        </Routes>
+      </div>
   );
 }
 
