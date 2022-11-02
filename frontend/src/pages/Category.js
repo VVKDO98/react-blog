3e976c7a1570d5e313c61d8c854547eb650d152f
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-02 10:56:35 +0100

    bug again with id

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const ARTICLES = gql`
+query GetCategory($id: ID!) { 
+	category(id: $id) { 
+        data{
+            id,
+            attributes{
+              name
+            },
+            articles{
+                title,
+                content,
+            }
+        }
+    }
+}
+`
 
 const Category = () => {
     return (
