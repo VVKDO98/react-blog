55550efc3fc899591ee8a24cd096064c5e683a98
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-02 10:18:59 +0100

    Add newcss links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
 import {Routes, Route} from "react-router-dom";
-import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
 
 import SiteHeader from "./components/SiteHeader";
 import Homepage from "./pages/Homepage";
 import ArticleDetails from "./pages/ArticleDetails";
 import Category from "./pages/Category";
 
-const client = new ApolloClient({
-  uri: 'http://localhost:1337/graphql',
-  cache: new InMemoryCache()
-})
-
 function App() {
   return (
-      <ApolloProvider client={client}>
         <div className="App">
           <SiteHeader/>
           <Routes>
@@ -22,7 +15,6 @@ function App() {
             <Route path="/category/:id" element={<Category/>}/>
           </Routes>
         </div>
-      </ApolloProvider>
   );
 }
 
