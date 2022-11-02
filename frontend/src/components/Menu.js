526ae1c9a5d2650fdddd6ab14d6b7f24bc55f1ac
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-02 15:41:26 +0100

    add menu on bottom

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import { useQuery, gql } from '@apollo/client';
+
+const CATEGORIES = gql`
+query GetArticles{ 
+	categories { 
+  	data{
+      id,
+      attributes{
+        name
+      }
+    }
+  }
+}
+`
+
+const Menu = () => {
+    const {loading, error, data} = useQuery(CATEGORIES);
+
+    if(loading) return <p>Loading navigation</p>
+    if(error) return <p>Error navigation</p>
+
+    console.log(data.categories.data);
+
+    return (
+        <div className='w-full h-20 flex flex-col justify-center items-center sticky bottom-0 bg-grey '>
+            <nav className='w-full'>
+                <ul className='flex flex-row justify-center gap-8'>
+                    {data.categories.data.map((category) => (
+                        <li>
+                            <Link key={category.id} to={`category/${category.id}`} className='text-green focus:text-red'>
+                                {category.attributes.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Menu;
