e8bef5fb572c6800307552acd64d1760640da605
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-03 14:16:51 +0100

    Category page responsive

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -24,7 +24,7 @@ const Menu = () => {
     console.log(data.categories.data);
 
     return (
-        <div className='w-full h-20 flex flex-col justify-center items-center fixed bottom-0 left-0 bg-grey outline outline-1 outline-green shadow-cust lg:top-0 lg:left-0 lg:w-24 lg:h-full'>
+        <div className='w-full h-20 flex flex-col justify-center items-center fixed bottom-0 left-0 bg-grey outline outline-1 outline-green shadow-cust lg:top-0 lg:left-0 lg:w-32 lg:h-full'>
             <nav className='w-full'>
                 <ul className='flex flex-row justify-center gap-8 lg:flex-col lg:items-center'>
                     <Link to="/" className='text-green focus:text-red selection:text-grey selection:bg-green'>Home</Link>
--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -46,10 +46,14 @@ const Category = () => {
         <div>
             <p className='text-base'>{data.category.data.attributes.name}</p>
             {data.category.data.attributes.articles.data.map((article) => (
-                <div key={article.id}>
-                    <h2 className='text-2xl text-green font-dmserif mb-3'>{article.attributes.title}</h2>
-                    <p className='text-base mb-3'>{article.attributes.content.substring(0, 200)}<Link to={`/details/${article.id}`} className='text-base text-green font-thin'>... read more</Link></p>
-                    <p className='text-base font-semibold'>{article.attributes.date}</p>
+                <div key={article.id} className='mb-5 flex flex-col lg:flex-row'>
+                  <div id='content' className='lg:order-2'>
+                    <h2 className='text-2xl text-green font-dmserif mb-3 lg:text-3xl lg:mb-5 selection:text-grey selection:bg-green'>{article.attributes.title}</h2>
+                    <p className='text-base mb-3 lg:text-xl text-justify selection:text-grey selection:bg-green'>{article.attributes.content.substring(0, 200)}<Link to={`/details/${article.id}`} className='text-base text-green font-thin'>... read more</Link></p>
+                  </div>
+                  <div id='date' className='lg:order-1 lg:mr-5'>
+                    <p className='text-base font-semibold lg:text-3xl selection:text-grey selection:bg-green'>{article.attributes.date}</p>
+                  </div>
                 </div>
             ))}
         </div>
