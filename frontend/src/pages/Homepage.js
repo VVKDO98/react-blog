a7eeb814bd37fb9b96778cf6ea27d1c5e890392f
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-03 10:47:43 +0100

    lg css progress

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import useFetch from "../hooks/useFetch";
 
 const Homepage = () => {
-    const {loading, error, data} = useFetch('http://localhost:1337/api/articles')
+    const {loading, error, data} = useFetch('http://localhost:1337/api/articles?sort=date:DESC')
 
     if(loading) return <p>Loading of articles</p>
     if(error) return <p>Unable to load articles</p>
@@ -13,10 +13,14 @@ const Homepage = () => {
     return (
         <div className='mb-40'>
             {data.map((article) => (
-                <div key={article.id} className="mb-5">
-                    <h2 className='text-2xl text-green font-dmserif mb-3 lg:text-3xl lg:mb-5'>{article.attributes.title}</h2>
-                    <p className='text-base mb-3 lg:text-xl'>{article.attributes.content.substring(0, 200)}<Link to={`/details/${article.id}`} className='text-base text-green font-thin lg:text-xl'> ... read more</Link></p>
-                    <p className='text-base font-semibold lg:text-xl'>{article.attributes.date}</p>
+                <div key={article.id} className="mb-5 flex flex-col lg:flex-row">
+                    <div id='content' className='lg:order-2'>
+                        <h2 className='text-2xl text-green font-dmserif mb-3 lg:text-3xl lg:mb-5'>{article.attributes.title}</h2>
+                        <p className='text-base mb-3 lg:text-xl'>{article.attributes.content.substring(0, 200)}<Link to={`/details/${article.id}`} className='text-base text-green font-thin lg:text-xl'> ... read more</Link></p>
+                    </div>
+                   <div id='date' className='lg:order-1 lg:mr-5'>
+                        <p className='text-base font-semibold lg:text-3xl'>{article.attributes.date}</p>
+                   </div>
                 </div>
             ))}
         </div>
